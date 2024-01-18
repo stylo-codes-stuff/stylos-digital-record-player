474d995bcc827df1999acf45bd404f9935a7396e
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-18 14:32:01 +0000

    for saving purposes

--- src/js/player.js
+++ src/js/player.js
@@ -1,12 +1,23 @@
-var sound = new Howl({
-    src: ['../songs/inception.mp3'],
-    autoplay: true,
-    loop: true,
-    volume: 0.5,
-    onend: function() {
-      console.log('Finished!');
-    }
-  });
-export function playSound(){
-    sound.play();
+var slider = document.getElementById("slider");
+var output = document.getElementById("sliderVal");
+var audio = document.getElementById('audio')
+var play_button = document.getElementById('play_button')
+var pause_button = document.getElementById('pause_button')
+var record = document.getElementById('vinyl')
+var pause = false;
+record.style.animation = `rotation 2s infinite`
+audio.preservesPitch = false;
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    audio.playbackRate = this.value/100
+    record.style.animation = `rotation ${100/this.value}s infinite linear`
+}
+play_button.addEventListener('click',()=>{
+    audio.play()
+})
+pause_button.addEventListener('click',()=>{
+    
+})
+while (pause == false){
+    audio.playbackRate = audio.playbackRate
 }
