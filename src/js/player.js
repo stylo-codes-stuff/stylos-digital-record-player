0d3e322556d110c002549aa5c3d21d010073c473
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-18 16:31:11 +0000

    added in fonts tailwinds and added a scroller for song choice

--- src/js/player.js
+++ src/js/player.js
@@ -4,13 +4,14 @@ var audio = document.getElementById('audio')
 var play_button = document.getElementById('play_button')
 var pause_button = document.getElementById('pause_button')
 var record = document.getElementById('vinyl')
+var thriller_play = document.getElementById('')
 var pause = false;
-record.style.animation = `rotation 2s infinite`
+record.style.animation = `rotation 2s infinite linear`
 audio.preservesPitch = false;
 slider.oninput = function() {
     output.innerHTML = this.value;
     audio.playbackRate = this.value/100
-    record.style.animation = `rotation ${100/this.value}s infinite linear`
+    record.style.animation = `rotation ${200/this.value}s infinite linear`
 }
 play_button.addEventListener('click',()=>{
     audio.play()
@@ -18,6 +19,10 @@ play_button.addEventListener('click',()=>{
 pause_button.addEventListener('click',()=>{
     
 })
-while (pause == false){
-    audio.playbackRate = audio.playbackRate
+while (pause == true){
+    while (audio.playbackRate > 0){
+    audio.playbackRate = audio.playbackRate - 2
+
+    }
+
 }
